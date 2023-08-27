2c0822d340d0d4b204e1321c4472512837300e14
septimiy-severus <131580499+septimiy-severus@users.noreply.github.com>
2023-08-27 12:24:01 +0300

    add style gallery

--- js/main.js
+++ js/main.js
@@ -6,4 +6,20 @@ $(document).ready(function () {
         $(".video-block").trigger("play")
     });
     $(".video-block").trigger("pause")
+
+    // up-arrow
+    $(window).scroll(function(){
+        if($(this).scrollTop()>200){
+            $(".btn-up").fadeIn(800)
+        } else{
+            $(".btn-up").fadeOut(800)
+        }
+    })
+
+    $(".btn-up").on("click", function(){
+        $("body, html").animate({
+            scrollTop: 0
+        }, 800)
+        return false;
+    })
 });
